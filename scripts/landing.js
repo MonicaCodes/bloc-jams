c446134aea46c852650f16ed429df7044bdfbb52
Monica Berkowitz <monicaberkowitz@gmail.com>
2017-07-19 17:15:26 -0400

    fixing animate point function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,14 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
-points.style.opacity = 1;
-points.style.transform = "scaleX(1) translateY(0)";
-points.style.msTransform = "scaleX(1) translateY(0)";
-points.style.WebkitTransform = "scaleX(1) translateY(0)";
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
+var animatePoints = function(myArray) {
+    forEach(myArray, revealPoint);
 };
 
 window.onload = function() {
@@ -16,9 +16,11 @@ window.onload = function() {
     if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-
+     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
      window.addEventListener("scroll", function(event) {
-         if (pointsArray[0].getBoundingClientRect().top <= 500) {
+         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);
          }
      });
+}
