e4a57270e2f0d8ee77c453dac1e6591a04258501
Monica Berkowitz <monicaberkowitz@gmail.com>
2017-07-19 16:54:50 -0400

    fixing animate points

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,16 +2,18 @@ var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
 
-var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
 
+
+    };
     for (var i = 0; i < points.length; i++) {
         revealPoint(i);
     }
-};
+}
 window.onload = function() {
 
     if (window.innerHeight > 950) {
@@ -25,3 +27,4 @@ window.onload = function() {
              animatePoints(pointsArray);
          }
      });
+ }
